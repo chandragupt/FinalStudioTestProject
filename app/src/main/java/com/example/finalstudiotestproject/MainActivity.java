503799b0329657dcbbb1b07ebc0fa79a62702eda
chandu <chandu01201@gmail.com>
2015-02-19 16:21:55 +0530

    commited

--- app/src/main/java/com/example/finalstudiotestproject/MainActivity.java
+++ app/src/main/java/com/example/finalstudiotestproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalstudiotestproject;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> data = new ArrayList<String>();
+        Log.d("","");
     }
 
 
